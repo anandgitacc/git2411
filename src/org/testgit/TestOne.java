59e814f56f57df4e460fb674222d62e9163c50e1
anandgitacc <anand.akmail@gmail.com>
2021-11-24 11:45:18 +0530

    firststage

---
+++ src/org/testgit/TestOne.java
@@ -0,0 +1,24 @@
+package org.testgit;
+
+public class TestOne {
+	
+public void empName() {
+		
+		System.out.println("anand");
+	}
+	
+	public void empMobno() {
+		
+		System.out.println("8939278836");
+	}
+	
+		
+public static void main(String[] args) {
+		
+	TestOne s = new TestOne();
+	
+	s.empName();
+	s.empMobno();
+}
+
+}
